b0bf2eb140fca9204a7858e502288ed9e94f8db0
Devak Gounden <100696124+AstralCloud-SZA@users.noreply.github.com>
2026-06-22 23:11:15 +0200

    git publishing integration work 5

--- crp56/electron-app/forge.config.js
+++ crp56/electron-app/forge.config.js
@@ -8,7 +8,7 @@ module.exports = {
         extraResource: [
             '../ruby-runtime',
             '../ruby-core',
-            '../soundengine'
+            './soundengine'
         ]
     },
 
